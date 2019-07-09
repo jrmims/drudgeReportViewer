7193df3f85762c1a8eabfc4ad9d44dca4e649d59
jim <14103155+jrmims@users.noreply.github.com>
2019-07-08 23:20:37 -0400

    feat: extracts urls and title for articles

--- app/src/main/java/dev/mims/drudgereportviewer/GrabHTMLTask.java
+++ app/src/main/java/dev/mims/drudgereportviewer/GrabHTMLTask.java
@@ -1,32 +1,45 @@
 package dev.mims.drudgereportviewer;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class GrabHTMLTask extends AsyncTask<URL, Integer, String> {
-    private String htmlStr;
-    public GrabHTMLTask(String htmlStr, AsyncResponse asyncResponse)
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class GrabHTMLTask extends AsyncTask<URL, Integer, Map<String, List<Map<String,String>>> > {
+    public GrabHTMLTask(Map<String, List<Map<String,String>>> resultMap, AsyncResponse asyncResponse)
     {
-        this.htmlStr = htmlStr;
+        this.resultMap = resultMap;
         this.asyncResponseInterface = asyncResponse;
     }
 
     public interface AsyncResponse {
-        void processFinish(String htmlString);
+        void processFinish(Map<String, List<Map<String,String>>> resultMap);
     }
 
     public AsyncResponse asyncResponseInterface = null;
+    private Map<String, List<Map<String,String>>> resultMap;
 
-    protected String doInBackground(URL... urls) {
-        int count = urls.length;
+    protected Map<String, List<Map<String,String>>> doInBackground(URL... urls) {
         BufferedReader reader;
         StringBuilder stringBuilder;
-        String htmlStr = "";
+
+        List<Map<String,String>> topLinkList = new ArrayList<>();
+        List<Map<String,String>> leftLinkList = new ArrayList<>();
+        List<Map<String,String>> middleLinkList = new ArrayList<>();
+        List<Map<String,String>> rightLinkList = new ArrayList<>();
         try {
             // Setup Connection
             URL url = urls[0];
@@ -42,19 +55,78 @@ public class GrabHTMLTask extends AsyncTask<URL, Integer, String> {
             {
                 stringBuilder.append(line + "\n");
             }
-            htmlStr = stringBuilder.toString();
+            String htmlStr = stringBuilder.toString();
             connection.disconnect();
+
+            // parse links
+            // find Top Links and Main Headline
+            // Located before first <td>
+            // find first <td>
+            int stopPos = htmlStr.toLowerCase().indexOf("<td");
+            String tempStr = htmlStr.substring(0,stopPos);
+            Document tempDoc = Jsoup.parse(tempStr);
+            Elements tempLinks = tempDoc.select("a");
+            topLinkList = extractInfoFromLinks(tempLinks);
+            /*
+            To grab left column links, we need to grab the first td table cell. Then we convert to
+            string, and find the <! which currently marks the start of the source links which we
+            don't want to display. We then convert it back to a Document and extract the links <a>
+             */
+            // in the first <td> section
+            tempDoc = Jsoup.parse(htmlStr);
+            Elements tableCells = tempDoc.select("td");
+            Element leftCol = tableCells.get(0);
+            leftLinkList = extractLinksFromTD(leftCol);
+            // middle column
+            Element middleCol = tableCells.get(2);
+            middleLinkList = extractLinksFromTD(middleCol);
+            // right column
+            Element rightCol = tableCells.get(4);
+            rightLinkList = extractLinksFromTD(rightCol);
 
         } catch (IOException e)
         {
-            htmlStr = e.toString();
+            Log.e("GrabHTMLTask", e.toString());
         }
+        Map<String, List<Map<String,String>>> columnLinksMap = new HashMap<String,List<Map<String,String>>>();
+        columnLinksMap.put("top", topLinkList);
+        columnLinksMap.put("left", leftLinkList);
+        columnLinksMap.put("middle", middleLinkList);
+        columnLinksMap.put("right", rightLinkList);
 
-        return htmlStr;
+        return columnLinksMap;
     }
 
-    protected void onPostExecute(String result) {
-        htmlStr = result;
-        asyncResponseInterface.processFinish(htmlStr);
+    protected void onPostExecute(Map<String, List<Map<String,String>>> result) {
+        resultMap = result;
+        asyncResponseInterface.processFinish(resultMap);
+    }
+
+    private List<Map<String,String>> extractInfoFromLinks(Elements links)
+    {
+        List<Map<String,String>> tempLinkList = new ArrayList<Map<String,String>>();
+        for (Element tempLink : links)
+        {
+            Map<String,String> tempLinkMap = new HashMap<String, String>();
+            tempLinkMap.put("title",tempLink.text());
+            tempLinkMap.put("url",tempLink.attr("href"));
+            tempLinkMap.put("img",""); // TODO
+            tempLinkList.add(tempLinkMap);
+        }
+        return tempLinkList;
+    }
+
+    private List<Map<String,String>> extractLinksFromTD(Element tableCell)
+    {
+        // in the first <td> section
+        String tempStr = tableCell.toString();
+        // filter out source links
+        int stopPos = tempStr.indexOf("<!"); // source links start after this comment section
+        tempStr = tempStr.substring(0,stopPos);
+        // all links found in tempStr should now be real news links
+        Document tempDoc = Jsoup.parseBodyFragment(tempStr);
+        Elements columnLinks = tempDoc.select("a");
+        List<Map<String,String>> linkList = extractInfoFromLinks(columnLinks);
+        return linkList;
     }
 }
