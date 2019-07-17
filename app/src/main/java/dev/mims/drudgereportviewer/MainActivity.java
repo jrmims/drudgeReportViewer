b02d12f157d02a2da6008c8a715f43a763c140cd
jim <14103155+jrmims@users.noreply.github.com>
2019-07-17 19:04:40 -0400

    feat: implement MainActivity and add linearLayout to fragment_main

--- app/src/main/java/dev/mims/drudgereportviewer/MainActivity.java
+++ app/src/main/java/dev/mims/drudgereportviewer/MainActivity.java
@@ -6,13 +6,19 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dev.mims.drudgereportviewer.ui.main.PageViewModel;
 import dev.mims.drudgereportviewer.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,12 +27,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        final PageViewModel pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        final SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        final ViewPager viewPager = findViewById(R.id.view_pager);
+
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
+        URL[] urlArray = new URL[1];
+        URL drudgeURL;
+        // initialize resultLinkMap
+        Map<String, List<Map<String,String>>> resultLinkMap = new HashMap<>();
+
+        // TODO make WeakReference
+        GrabHTMLTask linksGrabber = new GrabHTMLTask(resultLinkMap, new GrabHTMLTask.AsyncResponse() {
+            @Override
+            public void processFinish(Map<String, List<Map<String,String>>> resultMap) {
+                // Receives list/map of urls from GrabHTMLTask, pushes to PageViewModel for storage
+                pageViewModel.storeLinks(resultMap);
+            }
+        });
+        try {
+            // Set GrabHTMLTask to run
+            drudgeURL = new URL("http://www.drudgereport.com");
+            urlArray[0] = drudgeURL;
+            linksGrabber.execute(drudgeURL);
+        }
+        catch(MalformedURLException e)
+        {
+
+        }
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
